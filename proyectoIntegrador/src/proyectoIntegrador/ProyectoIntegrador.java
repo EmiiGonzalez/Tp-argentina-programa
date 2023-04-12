43be7c9acb9d85e425d6443d8f40db821d3f1695
GonzaloDeLaIglesia <gonzalo_895@yahoo.com.ar>
2023-04-12 14:46:52 -0300

    Creación y conexion a la base de datos

--- proyectoIntegrador/src/proyectoIntegrador/ProyectoIntegrador.java
+++ proyectoIntegrador/src/proyectoIntegrador/ProyectoIntegrador.java
@@ -1,12 +1,61 @@
 package proyectoIntegrador;
 
 import java.nio.file.Path;
+import java.sql.*;
 import java.nio.file.Paths;
 import java.util.Map;
 
 
 public class ProyectoIntegrador {
 	public static void main(String[] args) {
+		
+		try{  
+
+			Class.forName("org.postgresql.Driver");    
+			Connection con=DriverManager.getConnection("jdbc:postgresql://localhost/ProyectoIntegradorJava","postgres","ArgentinaPrograma");  
+			Statement stmt=con.createStatement();  
+			
+
+ 
+
+			//ResultSet rs=stmt.executeQuery("select codigo,denominacion,estado from color where denominacion like '%LILA%'");  
+
+			//while(rs.next())  
+
+			//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+
+ 
+
+			///int result=stmt.executeUpdate("insert into tabla1 (id,nombre) values (3,'Leonel')");  
+
+			//System.out.println(result + " records affected");  
+
+			
+
+			//ResultSet rs=stmt.executeQuery("select * from tabla1  ");  
+
+			
+
+			//ResultSetMetaData rsmd=rs.getMetaData();
+
+ 
+
+			
+
+			//while(rs.next())  
+
+			//System.out.println(rs.getInt(1)+"  "+rs.getString(2));
+
+			//	System.out.println(rsmd.getColumnCount());
+
+ 
+
+ 
+
+			con.close();  
+
+			} catch(Exception e){ System.out.println(e);}  
+		
 		// se generan variables con los archivos
 		System.out.println(System.getProperty("user.dir"));
 		Path resultado = Paths.get("src\\Archivos","resultados.txt");
